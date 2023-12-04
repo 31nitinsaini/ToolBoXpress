45e81a7a8ad0f7d36b46346598f4dc90280caf2c
31nitinsaini <101384128+31nitinsaini@users.noreply.github.com>
2023-12-05 01:15:30 +0530

    fix code

--- frontend/src/Pages/HashingTool/HMACGenerator.js
+++ frontend/src/Pages/HashingTool/HMACGenerator.js
@@ -1,82 +1,98 @@
-// import React, { useState } from 'react';
-// import { createHmac } from 'crypto';
-// import Header from '../../Components/Header';
-// import RatingComponent from '../../Components/RatingComponent';
-// import Footer from '../../Components/Footer';
+import React, { useState } from 'react';
+import Header from '../../Components/Header';
+import RatingComponent from '../../Components/RatingComponent';
+import Footer from '../../Components/Footer';
 
-// const HMACGenerator = () => {
-//     const [inputText, setInputText] = useState('');
-//     const [secretKey, setSecretKey] = useState('');
-//     const [hmacResult, setHMACResult] = useState('');
+const hmacGenerator = (key, message) => {
+    const blocksize = 64; // Block size for SHA-256
+    const ipad = 0x36;
+    const opad = 0x5c;
 
-//     const handleInputChange = (e) => {
-//         setInputText(e.target.value);
-//     };
+    // If the key is longer than the block size, hash it
+    if (key.length > blocksize) {
+        key = window.crypto.subtle.digest('SHA-256', new TextEncoder().encode(key));
+    }
 
-//     const handleKeyChange = (e) => {
-//         setSecretKey(e.target.value);
-//     };
+    // Pad the key with zeros to the block size
+    key = new Uint8Array([...key, ...Array(blocksize - key.length).fill(0)]);
 
-//     const generateHMAC = () => {
-//         if (!inputText || !secretKey) {
-//             alert('Please enter input text and secret key.');
-//             return;
-//         }
-    
-//         const hmac = createHmac('sha256', secretKey).update(inputText).digest('hex');
-//         setHMACResult(hmac);
-//     };
-    
+    // XOR key with inner and outer pads
+    const iKeyPad = key.map((byte) => byte ^ ipad);
+    const oKeyPad = key.map((byte) => byte ^ opad);
 
-//     return (
-//         <>
-//             <Header />
-//             <main>
-//                 <div className="container mt-5">
-//                     <h2 className="mb-4">HMAC Generator</h2>
-//                     <div className="mb-3">
-//                         <label htmlFor="inputText" className="form-label">Input Text:</label>
-//                         <textarea
-//                             id="inputText"
-//                             className="form-control"
-//                             value={inputText}
-//                             onChange={handleInputChange}
-//                             placeholder="Enter text to hash"
-//                         />
-//                     </div>
-//                     <div className="mb-3">
-//                         <label htmlFor="secretKey" className="form-label">Secret Key:</label>
-//                         <input
-//                             type="password"
-//                             id="secretKey"
-//                             className="form-control"
-//                             value={secretKey}
-//                             onChange={handleKeyChange}
-//                             placeholder="Enter secret key"
-//                         />
-//                     </div>
-//                     <button className="btn btn-primary" onClick={generateHMAC}>Generate HMAC</button>
-//                     {hmacResult && (
-//                         <div className="mt-4">
-//                             <h3>HMAC Result:</h3>
-//                             <p>{hmacResult}</p>
-//                         </div>
-//                     )}
-//                 </div>
-//             </main>
-//             <RatingComponent />
-//             <Footer />
-//         </>
-//     );
-// };
+    // Concatenate inner pad and message, then hash
+    const innerHash = window.crypto.subtle.digest('SHA-256', new Uint8Array([...iKeyPad, ...new TextEncoder().encode(message)]));
 
-// export default HMACGenerator;
-import React from 'react'
+    // Concatenate outer pad and inner hash, then hash again
+    const finalHash = window.crypto.subtle.digest('SHA-256', new Uint8Array([...oKeyPad, ...new Uint8Array(innerHash)]));
+
+    // Convert the final hash to a hex string
+    return Array.from(new Uint8Array(finalHash), (byte) => byte.toString(16).padStart(2, '0')).join('');
+};
 
 const HMACGenerator = () => {
-  return (
-    <div>HMACGenerator</div>
-  )
-}
+    const [inputText, setInputText] = useState('');
+    const [secretKey, setSecretKey] = useState('');
+    const [hmacResult, setHMACResult] = useState('');
 
-export default HMACGenerator
+    const handleInputChange = (e) => {
+        setInputText(e.target.value);
+    };
+
+    const handleKeyChange = (e) => {
+        setSecretKey(e.target.value);
+    };
+
+    const generateHMAC = () => {
+        if (!inputText || !secretKey) {
+            alert('Please enter input text and secret key.');
+            return;
+        }
+
+        const hmac = hmacGenerator(secretKey, inputText);
+        setHMACResult(hmac);
+    };
+
+    return (
+        <>
+            <Header />
+            <main>
+                <div className="container mt-5">
+                    <h2 className="mb-4">HMAC Generator</h2>
+                    <div className="mb-3">
+                        <label htmlFor="inputText" className="form-label">Input Text:</label>
+                        <textarea
+                            id="inputText"
+                            className="form-control"
+                            value={inputText}
+                            onChange={handleInputChange}
+                            placeholder="Enter text to hash"
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="secretKey" className="form-label">Secret Key:</label>
+                        <input
+                            type="password"
+                            id="secretKey"
+                            className="form-control"
+                            value={secretKey}
+                            onChange={handleKeyChange}
+                            placeholder="Enter secret key"
+                        />
+                    </div>
+                    <button className="btn btn-primary" onClick={generateHMAC}>Generate HMAC</button>
+                    {hmacResult && (
+                        <div className="mt-4">
+                            <h3>HMAC Result:</h3>
+                            <p>{hmacResult}</p>
+                        </div>
+                    )}
+                </div>
+            </main>
+            <RatingComponent />
+            <Footer />
+        </>
+    );
+};
+
+export default HMACGenerator;
