fd3b7e43ff4f8bfa0ae251fc7ef80bb1f3471552
31nitinsaini <101384128+31nitinsaini@users.noreply.github.com>
2023-12-05 00:46:46 +0530

    add new os tools

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,11 +1,13 @@
-const path = require('path');
+// config-overrides.js
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-    // Add the necessary polyfills
-    config.resolve.fallback = {
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-    };
+  // Add the necessary fallbacks for crypto and stream
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+  };
 
-    return config;
+  return config;
 };
