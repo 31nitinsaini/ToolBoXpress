fdc771686bc2c1dfa012a8432974fc67c88b1fa0
Nitin Saini <101384128+31nitinsaini@users.noreply.github.com>
2023-12-10 22:04:46 +0530

    Update CyrusBeckLineClipingAlgorithm.js

--- frontend/src/Pages/ComputerGraphicsTool/CyrusBeckLineClipingAlgorithm.js
+++ frontend/src/Pages/ComputerGraphicsTool/CyrusBeckLineClipingAlgorithm.js
@@ -1,118 +1,86 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Header from '../../Components/Header';
-import Footer from '../../Components/Footer';
-import RatingComponent from '../../Components/RatingComponent';
 import { Helmet } from 'react-helmet';
-import Typography from '@mui/material/Typography';
+import Header from '../../Components/Header';
+import RatingComponent from '../../Components/RatingComponent';
+import Footer from '../../Components/Footer';
 
-const CyrusBeckClippingTool = () => {
+const CyrusBeckLineClipingAlgorithm = () => {
   const canvasRef = useRef(null);
+  const [line, setLine] = useState({ x1: 50, y1: 100, x2: 200, y2: 300 });
+  const [clipWindow, setClipWindow] = useState({ xMin: 100, yMin: 150, xMax: 300, yMax: 250 });
 
-  const [x1, setX1] = useState(50);
-  const [y1, setY1] = useState(50);
-  const [x2, setX2] = useState(150);
-  const [y2, setY2] = useState(150);
+  const updateLine = (key, value) => {
+    setLine((prevLine) => ({ ...prevLine, [key]: value }));
+  };
 
-  const [clipXmin, setClipXmin] = useState(50);
-  const [clipYmin, setClipYmin] = useState(50);
-  const [clipXmax, setClipXmax] = useState(200);
-  const [clipYmax, setClipYmax] = useState(200);
+  const updateClipWindow = (key, value) => {
+    setClipWindow((prevWindow) => ({ ...prevWindow, [key]: value }));
+  };
 
-  const canvasWidth = 400;
-  const canvasHeight = 400;
+  const cyrusBeckLineClip = () => {
+    // Implement the Cyrus-Beck algorithm logic here
+    // Modify the line coordinates based on the clipping window
+    // This is a simplified example; you may need to adjust it based on your specific requirements
+    const dx = line.x2 - line.x1;
+    const dy = line.y2 - line.y1;
+
+    const p1 = -dx;
+    const p2 = dx;
+    const p3 = -dy;
+    const p4 = dy;
+
+    const q1 = line.x1 - clipWindow.xMin;
+    const q2 = clipWindow.xMax - line.x1;
+    const q3 = line.y1 - clipWindow.yMin;
+    const q4 = clipWindow.yMax - line.y1;
+
+    const t1 = q1 / p1;
+    const t2 = q2 / p2;
+    const t3 = q3 / p3;
+    const t4 = q4 / p4;
+
+    const tIn = Math.max(0, Math.max(t1, t3));
+    const tOut = Math.min(1, Math.min(t2, t4));
+
+    if (tIn <= tOut) {
+      const clippedX1 = line.x1 + tIn * dx;
+      const clippedY1 = line.y1 + tIn * dy;
+      const clippedX2 = line.x1 + tOut * dx;
+      const clippedY2 = line.y1 + tOut * dy;
+
+      // Update the line coordinates based on the clipping
+      setLine({ x1: clippedX1, y1: clippedY1, x2: clippedX2, y2: clippedY2 });
+
+      if (tIn === 0 && tOut === 1) {
+        alert('The entire line is within the clipping window.');
+      }
+    }
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-
-    // Clear the canvas
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw the clipping window
-    context.strokeRect(clipXmin, clipYmin, clipXmax - clipXmin, clipYmax - clipYmin);
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(clipWindow.xMin, clipWindow.yMin, clipWindow.xMax - clipWindow.xMin, clipWindow.yMax - clipWindow.yMin);
 
     // Draw the original line
-    context.beginPath();
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();
-  }, [x1, y1, x2, y2, clipXmin, clipYmin, clipXmax, clipYmax]);
+    ctx.strokeStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(line.x1, line.y1);
+    ctx.lineTo(line.x2, line.y2);
+    ctx.stroke();
 
-  const clipLineCyrusBeck = (context, x1, y1, x2, y2, clipXmin, clipYmin, clipXmax, clipYmax) => {
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-
-    const p1 = [-dx, -dy];
-    const p2 = [dx, dy];
-
-    const normals = [
-      [0, -1],  // top
-      [1, 0],   // right
-      [0, 1],   // bottom
-      [-1, 0],  // left
-    ];
-
-    let tEnter = 0;
-    let tExit = 1;
-
-    for (let i = 0; i < normals.length; i++) {
-      const N = normals[i];
-      const D = [x1 - clipXmin, clipYmin - y1];
-      const num = N[0] * D[0] + N[1] * D[1];
-      const den = N[0] * (p1[0] - p2[0]) + N[1] * (p1[1] - p2[1]);
-
-      if (den === 0) {
-        if (num < 0) {
-          return;
-        }
-      } else {
-        const t = -num / den;
-        if (den > 0) {
-          tEnter = Math.max(tEnter, t);
-        } else {
-          tExit = Math.min(tExit, t);
-        }
-      }
-    }
-
-    if (tEnter <= tExit) {
-      const new_x1 = x1 + tEnter * p1[0];
-      const new_y1 = y1 + tEnter * p1[1];
-      const new_x2 = x1 + tExit * p1[0];
-      const new_y2 = y1 + tExit * p1[1];
-
-      // Draw the clipped line
-      context.beginPath();
-      context.moveTo(new_x1, new_y1);
-      context.lineTo(new_x2, new_y2);
-      context.strokeStyle = 'green';
-      context.stroke();
-
-      // Alert the new coordinates
-      alert(`New Coordinates: (${new_x1}, ${new_y1}) to (${new_x2}, ${new_y2})`);
-    }
-  };
-
-  const handleClipLine = () => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-
-    // Clear the canvas
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw the clipping window
-    context.strokeRect(clipXmin, clipYmin, clipXmax - clipXmin, clipYmax - clipYmin);
-
-    // Draw the original line
-    context.beginPath();
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.strokeStyle = 'black'; // Reset stroke style
-    context.stroke();
-
-    // Draw the clipped line using Cyrus-Beck algorithm
-    clipLineCyrusBeck(context, x1, y1, x2, y2, clipXmin, clipYmin, clipXmax, clipYmax);
-  };
+    // Draw the clipped line
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(line.x1, line.y1);
+    ctx.lineTo(line.x2, line.y2);
+    ctx.stroke();
+  }, [line, clipWindow]);
 
   return (
     <>
@@ -127,79 +95,71 @@ const CyrusBeckClippingTool = () => {
       <main>
         <div className="container my-5">
           <center>
-          <Typography variant="h2" className="mb-4">
-            Cyrus-Beck Clipping
-          </Typography>
-          <Typography variant="body1" paragraph>
-            Use the Cyrus-Beck line clipping algorithm to clip a line segment against a rectangular clipping window.
-            Enter the coordinates of the line and dimensions of the clipping window below.
-          </Typography>
+            <h2>Cyrus-Beck Line Clipping</h2>
+            <p>
+              Use the Cyrus-Beck algorithm to efficiently clip lines against a rectangular clipping window. Enter the coordinates of the line and dimensions of the clipping window below.
+            </p>
           </center>
-
-          <div className="row mt-4">
-            <div className="col">
-              <Typography variant="h4" gutterBottom>
-                Steps for Cyrus-Beck Clipping Algorithm:
-              </Typography>
+          <div className="col">
+              <h3>Steps for Cyrus-Beck Line Clipping Algorithm Steps</h3>
               <ol>
-                <li>Identify the coordinates of the line segment's endpoints (x1, y1) and (x2, y2).</li>
-                <li>Define the rectangular clipping window by specifying its boundaries (Xmin, Ymin, Xmax, Ymax).</li>
-                <li>Calculate the direction vector (dx, dy) of the line segment.</li>
-                <li>Calculate the normal vectors of the clipping window.</li>
-                <li>Compute the intersection points of the line with the window using the Cyrus-Beck algorithm.</li>
-                <li>Draw the clipped line segment based on the computed intersection points.</li>
+                <li>Identify the coordinates of the line segment's endpoints: (x1, y1) and (x2, y2).</li>
+                <li>Define the rectangular clipping window by specifying its boundaries: (XMin, YMin, XMax, YMax).</li>
+                <li>Calculate the direction vector of the line: dx = x2 - x1, dy = y2 - y1.</li>
+                <li>Calculate the parameters p1, p2, p3, p4, q1, q2, q3, and q4 as per the Cyrus-Beck algorithm.</li>
+                <li>Calculate the values of t1, t2, t3, and t4.</li>
+                <li>Determine the intersection points using the calculated t values.</li>
+                <li>If tIn is less than or equal to tOut, update the line coordinates based on the intersection points.</li>
               </ol>
             </div>
-          </div>
-
-
-          <div className="row">
+          <div className="row mt-4">
             <div className="col-md-6">
               <div className="form-group">
                 <label>Line Coordinates:</label>
                 <div className="row">
                   <div className="col">
                     <label>X1:</label>
-                    <input type="number" className="form-control" value={x1} onChange={(e) => setX1(parseInt(e.target.value))} />
+                    <input type="number" className="form-control" value={line.x1} onChange={(e) => updateLine('x1', parseInt(e.target.value, 10))} />
                   </div>
                   <div className="col">
                     <label>Y1:</label>
-                    <input type="number" className="form-control" value={y1} onChange={(e) => setY1(parseInt(e.target.value))} />
+                    <input type="number" className="form-control" value={line.y1} onChange={(e) => updateLine('y1', parseInt(e.target.value, 10))} />
                   </div>
                 </div>
                 <div className="row mt-3">
                   <div className="col">
                     <label>X2:</label>
-                    <input type="number" className="form-control" value={x2} onChange={(e) => setX2(parseInt(e.target.value))} />
+                    <input type="number" className="form-control" value={line.x2} onChange={(e) => updateLine('x2', parseInt(e.target.value, 10))} />
                   </div>
                   <div className="col">
                     <label>Y2:</label>
-                    <input type="number" className="form-control" value={y2} onChange={(e) => setY2(parseInt(e.target.value))} />
+                    <input type="number" className="form-control" value={line.y2} onChange={(e) => updateLine('y2', parseInt(e.target.value, 10))} />
                   </div>
                 </div>
               </div>
             </div>
+
             <div className="col-md-6">
               <div className="form-group">
                 <label>Clip Window:</label>
                 <div className="row">
                   <div className="col">
-                    <label>Xmin:</label>
-                    <input type="number" className="form-control" value={clipXmin} onChange={(e) => setClipXmin(parseInt(e.target.value))} />
+                    <label>XMin:</label>
+                    <input type="number" className="form-control" value={clipWindow.xMin} onChange={(e) => updateClipWindow('xMin', parseInt(e.target.value, 10))} />
                   </div>
                   <div className="col">
-                    <label>Ymin:</label>
-                    <input type="number" className="form-control" value={clipYmin} onChange={(e) => setClipYmin(parseInt(e.target.value))} />
+                    <label>YMin:</label>
+                    <input type="number" className="form-control" value={clipWindow.yMin} onChange={(e) => updateClipWindow('yMin', parseInt(e.target.value, 10))} />
                   </div>
                 </div>
                 <div className="row mt-3">
                   <div className="col">
-                    <label>Xmax:</label>
-                    <input type="number" className="form-control" value={clipXmax} onChange={(e) => setClipXmax(parseInt(e.target.value))} />
+                    <label>XMax:</label>
+                    <input type="number" className="form-control" value={clipWindow.xMax} onChange={(e) => updateClipWindow('xMax', parseInt(e.target.value, 10))} />
                   </div>
                   <div className="col">
-                    <label>Ymax:</label>
-                    <input type="number" className="form-control" value={clipYmax} onChange={(e) => setClipYmax(parseInt(e.target.value))} />
+                    <label>YMax:</label>
+                    <input type="number" className="form-control" value={clipWindow.yMax} onChange={(e) => updateClipWindow('yMax', parseInt(e.target.value, 10))} />
                   </div>
                 </div>
               </div>
@@ -208,14 +168,15 @@ const CyrusBeckClippingTool = () => {
 
           <div className="row mt-3">
             <div className="col-md-6">
-              <button className="btn btn-primary" onClick={handleClipLine}>
-                <span>Clip Line</span>
-              </button>
+              <canvas ref={canvasRef} width={400} height={400} style={{ border: '1px solid black' }} />
             </div>
           </div>
+
           <div className="row mt-3">
-            <div className="col-md-6">
-              <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} className="border"></canvas>
+            <div className="col">
+              <button className="btn btn-primary" onClick={cyrusBeckLineClip}>
+                Clip Line
+              </button>
             </div>
           </div>
         </div>
@@ -226,4 +187,4 @@ const CyrusBeckClippingTool = () => {
   );
 };
 
-export default CyrusBeckClippingTool;
+export default CyrusBeckLineClipingAlgorithm;
